45cb3536c76e97c1cde040515d48f7bb789be3fb
joao-p-marques <joao.marques.contacto@gmail.com>
2018-06-01 18:07:22 +0100

    Project Page completed. Added option for follow/unfollow on the same page. loginData update.

--- scripts/project.js
+++ scripts/project.js
@@ -5,8 +5,9 @@ function viewModel() {
 
     var projectsData = [];
     self.projectId = ko.observable();
+    self.userId = ko.observable();
 
-    if (localStorage.projectsData) {
+    if (!localStorage.projectsData) {
         var ajaxData = [];
 
         $.ajax({
@@ -27,15 +28,58 @@ function viewModel() {
     
     projectsData = JSON.parse(localStorage.projectsData);
 
+    var loginData = [];
+    
+    if (!localStorage.loginData) {
+        var ajaxData = [];
+
+        $.ajax({
+            dataType: "json",
+            async : false,
+            /* mimeType: "application/json", */
+            url: "https://rawgit.com/joao-p-marques/2GoCommunity/master/loginData.json",
+            success: function(data){
+                console.log("JSON Data: " + data);
+                $.each(data, function(i, objectR){
+                    ajaxData.push(objectR);
+                })
+            }
+        })
+
+        localStorage.setItem("loginData", JSON.stringify(ajaxData));
+    }
+    
+    loginData = JSON.parse(localStorage.loginData);
+
     function processForm(){
         var queryString = decodeURIComponent(window.location.search);
-        var projectIdStr = queryString.substring(1);
+        
+        if(queryString.includes("&")){
+            var valuesString = queryString.substring(1).split("&");
+            var userIdStr = valuesString[0]
+            var projectIdStr = valuesString[1];
 
-        self.projectId(projectIdStr.split("=")[1]);
-        console.log("projectId = " + self.projectId());
+            self.userId(userIdStr.split("=")[1]);
+            self.projectId(projectIdStr.split("=")[1]);
+
+            console.log("userId = " + self.userId());
+            console.log("projectId = " + self.projectId());
+        }
+        else{
+            var projectIdStr = queryString.substring(1);
+            self.projectId(projectIdStr.split("=")[1]);
+            console.log("projectId = " + self.projectId());
+            $("#followButton").attr("disabled", true);
+        }
     }
 
     processForm();
+
+    console.log(loginData[self.userId()]);
+
+    if(loginData[self.userId()].followed.includes(parseInt(self.projectId()))){
+        $("#followButton").html("Unfollow");
+    }
 
     console.log(projectsData);
 
@@ -61,6 +105,24 @@ function viewModel() {
     self.project(findProject());
 
     console.log(self.project());
+
+    follow = function(){
+        console.log("In function (Un)follow...");
+        console.log($("#followButton").html());
+        if($("#followButton").html()=="Follow"){
+            loginData[self.userId()].followed.push(parseInt(self.projectId()));
+            localStorage.loginData = JSON.stringify(loginData);
+            $("#followButton").html("Unfollow");
+        }
+        else{
+            for(var i=0; i<loginData[self.userId()].followed.length; i++){
+                if(loginData[self.userId()].followed[i] == self.projectId()) loginData[self.userId()].followed.splice(i, 1);
+            }
+            localStorage.loginData = JSON.stringify(loginData);
+            $("#followButton").html("Follow");
+        }
+        console.log(loginData[self.userId()].followed);
+    }
 
 }
 
